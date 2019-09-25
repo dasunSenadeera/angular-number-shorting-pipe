387fa18a7b84cf65480030764353797519d155d1
dasunSenadeera <dasunms25@gmail.com>
2019-09-25 11:09:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-number-shorting-pipe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-number-shorting-pipe)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  a = 5;
+  b = 5555;
+  c = 5555555;
+  d = 5555555555;
+  e = 5555555555555;
+}
